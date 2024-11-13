60ce30d2cc954b8a1247301a42ca917dabab66d3
Sami kamran <130062857+Sami-Kamran-Sly@users.noreply.github.com>
2024-11-13 15:06:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://e-commerce-website-mern-app-frontend.vercel.app"],
     credentials: true,
   })
 );
